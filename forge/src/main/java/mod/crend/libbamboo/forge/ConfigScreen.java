1a5a7153e92dd3ae9936c40c67ec2b3dcf84f758
Crendgrim <Crendgrim@users.noreply.github.com>
2024-11-29 14:53:40 +0100

    Forge: use a Supplier to load config handler
    This prevents an issue where items are not loaded in time for the config
    screen handler registration, resulting in other mods' items in a config
    using ItemOrTag (or BlockOrTag) being replaced by minecraft:air on load.

--- forge/src/main/java/mod/crend/libbamboo/forge/ConfigScreen.java
+++ forge/src/main/java/mod/crend/libbamboo/forge/ConfigScreen.java
@@ -5,12 +5,19 @@ import mod.crend.libbamboo.opt.ConfigStore;
 import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.fml.ModLoadingContext;
 
+import java.util.function.Supplier;
+
 public class ConfigScreen {
+	@Deprecated
 	public static <T> void register(ModLoadingContext context, ConfigStore<T> configStore) {
+		register(context, () -> configStore);
+	}
+
+	public static <T> void register(ModLoadingContext context, Supplier<ConfigStore<T>> configStore) {
 		if (LibBamboo.HAS_YACL) {
 			context.registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class,
 					() -> new ConfigScreenHandler.ConfigScreenFactory(
-							(minecraft, screen) -> configStore.makeScreen(screen)
+							(minecraft, screen) -> configStore.get().makeScreen(screen)
 					));
 		}
 	}
